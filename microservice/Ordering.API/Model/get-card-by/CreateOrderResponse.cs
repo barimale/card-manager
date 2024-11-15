1ed9e9675e089664c70369f2bf73a6c37f40ec76
Mateusz <mateusz.wolnica@gmail.com>
2024-11-15 22:13:20 +0100

    .

---
+++ microservice/Ordering.API/Model/get-card-by/CreateOrderResponse.cs
@@ -0,0 +1,20 @@
+﻿using TypeGen.Core.TypeAnnotations;
+
+namespace Ordering.API.Model.order
+{
+    [ExportTsInterface]
+    public class CreateOrderResponse
+    {
+        public CreateOrderResponse()
+        {
+            // intentionally left blank
+        }
+
+        public CreateOrderResponse(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; set; }
+    }
+}
---
+++ microservice/Ordering.API/Model/get-card-by/RegisterCardRequest.cs
@@ -0,0 +1,16 @@
+﻿using BuildingBlocks.Application.CQRS;
+
+namespace Ordering.Application.CQRS.Commands
+{
+    public class RegisterCardRequest
+    {
+        public RegisterCardRequest()
+        {
+            //intentionally left blank
+        }
+
+        public string AccountNumber { get; set; }
+        public string PIN { get; set; }
+        public string SerialNumber { get; set; }
+    }
+}
