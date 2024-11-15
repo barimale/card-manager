3e80018ac75900725c15285d7227c26d5955f5f5
Mateusz <mateusz.wolnica@gmail.com>
2024-11-15 16:38:06 +0100

    .

--- ui/card-manager-ui/Services/IDataService.cs
+++ ui/card-manager-ui/Services/IDataService.cs
@@ -1,9 +1,15 @@
 ﻿namespace card_manager_ui.Services
 {
-    public interface IDataService
+    public interface IDataService : IDataReadService, IDataWriteService { }
+    public interface IDataReadService
     {
         ValueTask<dynamic> GetBySerialNumber(string value);
         ValueTask<dynamic> GetByIdentifier(string value);
-        ValueTask<dynamic> GetByAccountNumber(string value);
+        ValueTask<dynamic> GetByAccountNumber(string value);    }
+
+    public interface IDataWriteService
+    {
+        bool Create(string accountNumber, string serialNumber, string PIN);
+        bool Delete(string id);
     }
 }
