0d0594886b8ccd8a95abd2c58d5f4c917a49c72a
Mateusz <mateusz.wolnica@gmail.com>
2024-11-16 13:24:55 +0100

    .

---
+++ microservice/Ordering.API/Model/get-card-by/DeleteCardResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace Ordering.API.Model.order
+{
+    public class GetCardResponse
+    {
+        public GetCardResponse()
+        {
+            // intentionally left blank
+        }
+
+        public int Id { get; set; }
+        public int AccountNumber { get; set; }
+        public int SerialNumber { get; set; }
+        public int PIN { get; set; }
+        public DateTime CreationTime { get; set; }
+    }
+}
