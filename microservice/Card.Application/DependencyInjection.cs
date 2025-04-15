3925345acd5864879c35234ca5ba004adb7d4ee4
Mateusz <mateusz.wolnica@gmail.com>
2025-04-15 17:58:29 +0200

    .

--- microservice/Card.Application/DependencyInjection.cs
+++ microservice/Card.Application/DependencyInjection.cs
@@ -21,7 +21,7 @@ public static class DependencyInjection
             config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
             config.AddOpenBehavior(typeof(LoggingBehavior<,>));
             config.AddOpenBehavior(typeof(ValidationBehavior<,>));
-\           config.AddOpenBehavior(typeof(TransactionBehavior<,>));
+            config.AddOpenBehavior(typeof(TransactionBehavior<,>));
         });
 
         services.AddFeatureManagement();
