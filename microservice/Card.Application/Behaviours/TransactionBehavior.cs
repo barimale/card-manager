3d9ccac2b6b0380bb875ab4f683de5c7911c4fbe
Mateusz <mateusz.wolnica@gmail.com>
2025-04-22 14:28:45 +0200

    .

--- microservice/Card.Application/Behaviours/TransactionBehavior.cs
+++ microservice/Card.Application/Behaviours/TransactionBehavior.cs
@@ -1,63 +1,60 @@
-﻿namespace Card.Application.Behaviours;
-
-using Card.Application.Extensions;
+﻿using Card.Application.Extensions;
 using Card.Infrastructure;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
-public class TransactionBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+namespace Card.Application.Behaviours
 {
-    private readonly ILogger<TransactionBehavior<TRequest, TResponse>> _logger;
-    private readonly CardContext _dbContext;
-
-    public TransactionBehavior(CardContext dbContext,
-        ILogger<TransactionBehavior<TRequest, TResponse>> logger)
+    public class TransactionBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
     {
-        _dbContext = dbContext ?? throw new ArgumentException(nameof(CardContext));
-        _logger = logger ?? throw new ArgumentException(nameof(ILogger));
-    }
+        private readonly ILogger<TransactionBehavior<TRequest, TResponse>> _logger;
+        private readonly CardContext _dbContext;
 
-    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
-    {
-        var response = default(TResponse);
-        var typeName = request.GetGenericTypeName();
-
-        try
+        public TransactionBehavior(CardContext dbContext,
+            ILogger<TransactionBehavior<TRequest, TResponse>> logger)
         {
-            if (_dbContext.HasActiveTransaction)
-            {
-                return await next();
-            }
-
-            var strategy = _dbContext.Database.CreateExecutionStrategy();
-
-            await strategy.ExecuteAsync(async () =>
-            {
-                Guid transactionId;
-
-                await using var transaction = await _dbContext.BeginTransactionAsync(cancellationToken);
-                using (_logger.BeginScope(new List<KeyValuePair<string, object>> { new("TransactionContext", transaction.TransactionId) }))
-                {
-                    _logger.LogInformation("Begin transaction {TransactionId} for {CommandName} ({@Command})", transaction.TransactionId, typeName, request);
-
-                    response = await next();
-
-                    _logger.LogInformation("Commit transaction {TransactionId} for {CommandName}", transaction.TransactionId, typeName);
-
-                    await _dbContext.CommitTransactionAsync(transaction, cancellationToken);
-
-                    transactionId = transaction.TransactionId;
-                }
-            });
-
-            return response;
+            _dbContext = dbContext ?? throw new ArgumentException(nameof(CardContext));
+            _logger = logger ?? throw new ArgumentException(nameof(ILogger));
         }
-        catch (Exception ex)
-        {
-            _logger.LogError(ex, "Error Handling transaction for {CommandName} ({@Command})", typeName, request);
 
-            throw;
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var response = default(TResponse);
+            var typeName = request.GetGenericTypeName();
+
+            try
+            {
+                if (_dbContext.HasActiveTransaction)
+                {
+                    return await next();
+                }
+
+                var strategy = _dbContext.Database.CreateExecutionStrategy();
+
+                await strategy.ExecuteAsync(async () =>
+                {
+                    await using var transaction = await _dbContext.BeginTransactionAsync(cancellationToken);
+                    using (_logger.BeginScope(new List<KeyValuePair<string, object>> { new("TransactionContext", transaction.TransactionId) }))
+                    {
+                        _logger.LogInformation("Begin transaction {TransactionId} for {CommandName} ({@Command})", transaction.TransactionId, typeName, request);
+
+                        response = await next();
+
+                        _logger.LogInformation("Commit transaction {TransactionId} for {CommandName}", transaction.TransactionId, typeName);
+
+                        await _dbContext.CommitTransactionAsync(transaction, cancellationToken);
+                    }
+                });
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error Handling transaction for {CommandName} ({@Command})", typeName, request);
+
+                throw;
+            }
         }
     }
 }
